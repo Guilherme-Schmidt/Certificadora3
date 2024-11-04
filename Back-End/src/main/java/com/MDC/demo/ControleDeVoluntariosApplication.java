3a0e72ce3eec1220d0103f9b78c0778728ba8274
Guilherme Schmidt <devguilhermeschmidt@gmail.com>
2024-11-04 15:13:00 -0300

    Projeto Inicial

---
+++ Back-End/src/main/java/com/MDC/demo/ControleDeVoluntariosApplication.java
@@ -0,0 +1,13 @@
+package com.MDC.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVoluntariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVoluntariosApplication.class, args);
+	}
+
+}
