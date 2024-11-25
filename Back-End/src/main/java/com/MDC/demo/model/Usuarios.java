f5135bbb5fd2122f3a1d86e9b99432e88a3d1a69
Guilherme Schmidt <guischmidt201@gmail.com>
2024-11-25 14:51:59 -0300

    feat: criacao classe usuario

---
+++ Back-End/src/main/java/com/MDC/demo/model/Usuarios.java
@@ -0,0 +1,36 @@
+package com.MDC.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import org.hibernate.annotations.CollectionId;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+
+@Entity(name="usuarios")
+@Data
+public class Usuarios {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID_Usuario")
+    private Long id;
+    @Column(name="Nome",nullable = false,length = 100)
+    private String nome;
+    @Column(name="Funcao",length = 50)
+    private String funcao;
+    @Column(name="Setor",length = 50)
+    private String setor;
+    @Column(name="Data_Entrada",nullable = false)
+    private LocalDate dataEntrada;
+    @Column(name="Data_Saida")
+    private LocalDate dataSaida;
+    @ManyToOne
+    @JoinColumn(name="Permissao",nullable = false)
+    private Permissao permissao;
+    @Column(name="Email",nullable = false,unique = true,length = 100)
+    private String email;
+    @Column(name="Senha",nullable = false,length = 100)
+    private String senha;
+}
